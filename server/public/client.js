a98567bd619a69802660741945513bbb162d0d45
Kelsey Brown <kbrown55347@gmail.com>
2021-11-19 14:01:14 -0600

    created, wired and tested function to bundle input values into object on click of button

--- server/public/client.js
+++ server/public/client.js
@@ -1,7 +1,18 @@
-// console.log('js is sourced');
-
 $(document).ready(onReady);
 
 function onReady() {
-    // console.log('jQuery is sourced');
-}
+    // call function to get values on click of 'add task' button
+    $('#add-btn').on('click', handleAddTask);
+};
+
+
+
+// create function to pull values from input fields
+function handleAddTask() {
+    let taskToAdd = {
+        name: $('#task-name-in').val(),
+        description: $('#descript-in').val(),
+        date: $('#date-in').val()
+    }
+    console.log('In handleAddTask', taskToAdd);
+} // end handleAddTask
